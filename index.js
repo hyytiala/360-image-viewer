c7cbefa1699dd758611b25077aa05ebe09ef264e
Otto Hyytiälä <otto.hyytiala@gmail.com>
2018-12-17 17:24:26 +0200

    cleanup files

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ const styles = StyleSheet.create({
     width: 1000,
   },
   controls: {
-    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    backgroundColor: 'rgba(37, 50, 100, 0.7)',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
@@ -99,14 +99,14 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   button: {
-    backgroundColor: '#c0c0d0',
+    backgroundColor: '#27AAE1',
     borderRadius: 5,
     width: 50,
     height: 50,
   },
   buttonText: {
     textAlign: 'center',
-    color: '#000000',
+    color: '#253264',
     fontSize: 30,
     fontWeight: 'bold',
   },
